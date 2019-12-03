acd3a91bd353dd6d1ae02c9f813f3d64d09ac807
Thibaud Colas <thibaudcolas@gmail.com>
2019-12-03 13:07:58 +0000

    Add requirements for sue-ryder/bereavement stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,51 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// Load all files ending in .stories.js.
+const req = require.context('../src', true, /\.stories\.js$/);
+
+configure(() => {
+    /* eslint-disable global-require */
+    const hasIcons = document.querySelector('[data-storybook-svg-icons]');
+
+    if (!hasIcons) {
+        const icons = document.createElement('div');
+        icons.setAttribute('data-storybook-svg-icons', true);
+        icons.innerHTML = STORYBOOK_SVG_ICONS;
+        document.body.appendChild(icons);
+    }
+
+    window.CANCELLATION_HOURS = 48;
+
+    window.VIDEO_SETTINGS = {
+        video_call_period_start_padding: 0,
+        video_call_period_end_delay: 0,
+    };
+
+    window.SYSTEM_SETTINGS = {
+        title_404: 'Page not found',
+        body_404: '<p>Page not found</p>',
+        service_landing_url: 'https://www.example.com/#service_landing_url',
+        service_donation_url: 'https://www.example.com/#service_donation_url',
+        service_faq_url: 'https://www.example.com/#service_faq_url',
+        service_terms_url: 'https://www.example.com/#service_terms_url',
+        service_crisis_url: 'https://www.example.com/#service_crisis_url',
+        service_troubleshooting_url:
+            'https://www.example.com/#service_troubleshooting_url',
+        service_contact_email: 'test@example.com',
+        counsellor_resources_url:
+            'https://www.example.com/#counsellor_resources_url',
+    };
+
+    window.URL_PATTERNS = {
+        login: '/login',
+        logout: '/logout',
+        dashboard_index: '/dashboard_index',
+        dashboard_user_form: '/dashboard_user_form',
+        counsellor_dashboard_index: '/counsellor_dashboard_index',
+        counsellor_dashboard_caseload: '/counsellor_dashboard_caseload',
+    };
+
+    global.__webpack_hash__ = '13b43433ad2a439d00b2';
+
+    req.keys().forEach((filename) => req(filename));
+}, module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const sass = require('sass');
+
+const spritesPath = path.join(__dirname, 'sprites.html');
+const SVG_ICONS = fs.readFileSync(spritesPath, 'utf-8');
+
+module.exports = ({ config, mode }) => {
+    const isProduction = mode === 'PRODUCTION';
+    const sourceMap = !isProduction;
+
+    config.devtool = sourceMap ? 'cheap-module-source-map' : 'none';
+
+    const rules = [
+        {
+            test: /\.(scss|css)$/,
+            use: [
+                {
+                    loader: 'style-loader',
+                    options: {
+                        sourceMap,
+                    },
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap,
+                    },
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap,
+                        plugins: () => [
+                            autoprefixer(),
+                            cssnano({
+                                preset: 'default',
+                            }),
+                        ],
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap,
+                        implementation: sass,
+                    },
+                },
+            ],
+        },
+        {
+            test: /\.(ttf|woff|woff2|png|svg|jpeg)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            },
+        },
+    ];
+
+    config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            STORYBOOK_SVG_ICONS: JSON.stringify(SVG_ICONS),
+        }),
+    );
+
+    return config;
+};
