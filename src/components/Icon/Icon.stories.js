d5bd1a22da1122c41d9b869001007e96929031ef
Thibaud Colas <thibaudcolas@gmail.com>
2019-12-03 13:05:43 +0000

    Add Icon component from sue-ryder/bereavement

---
+++ src/components/Icon/Icon.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import Icon from './Icon';
+
+const ALL_ICONS = [
+    ...document.querySelectorAll('[data-storybook-svg-icons] symbol'),
+]
+    .map((symbol) => symbol.id)
+    .sort();
+
+storiesOf('Icon', module).add('All', () => (
+    <table>
+        <caption>All available icons</caption>
+        <tbody>
+            {ALL_ICONS.map((icon) => (
+                <tr key={icon}>
+                    <td>
+                        <Icon name={icon} />
+                    </td>
+                    <td>
+                        <code>{icon}</code>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+));
