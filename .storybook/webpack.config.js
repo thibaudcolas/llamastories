ce66fb6d1d4a7a99c236cf8614ad605ae0f7d7f0
Thibaud Colas <thibaudcolas@gmail.com>
2019-12-11 10:08:41 +0000

    Add Storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -69,5 +69,11 @@ module.exports = ({ config, mode }) => {
         }),
     );
 
+    config.module.rules.push({
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/source-loader')],
+        enforce: 'pre',
+    });
+
     return config;
 };
