df8a6136270455e45d2e5f583014ab7ea74c5873
Thibaud Colas <thibaudcolas@gmail.com>
2019-12-11 10:47:02 +0000

    Add example of addons - actions

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,31 +1,66 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 
 import Button, { BUTTON_THEMES } from './Button';
 
-storiesOf('Button', module).add('Reference', () => (
-    <>
-        <p>Buttons:</p>
-        {BUTTON_THEMES.map((theme) => (
-            <Button
-                key={theme}
-                theme={theme}
-                icon={theme.includes('circle') ? 'microphone' : null}
-            >
-                {theme.includes('circle') ? null : theme}
+storiesOf('Button', module)
+    .add('Reference', () => (
+        <>
+            <p>Buttons:</p>
+            {BUTTON_THEMES.map((theme) => (
+                <Button
+                    key={theme}
+                    theme={theme}
+                    icon={theme.includes('circle') ? 'microphone' : null}
+                >
+                    {theme.includes('circle') ? null : theme}
+                </Button>
+            ))}
+            <p>Link buttons:</p>
+            {BUTTON_THEMES.map((theme) => (
+                <Button
+                    to={theme}
+                    key={`to-${theme}`}
+                    theme={theme}
+                    title={theme}
+                    icon={theme.includes('circle') ? 'microphone' : null}
+                >
+                    {theme.includes('circle') ? null : theme}
+                </Button>
+            ))}
+        </>
+    ))
+    .add('Button icons', () => (
+        <>
+            <p>Icon before:</p>
+            <Button theme="secondary" iconBefore="off-site">
+                View calendar
             </Button>
-        ))}
-        <p>Link buttons:</p>
-        {BUTTON_THEMES.map((theme) => (
-            <Button
-                to={theme}
-                key={`to-${theme}`}
-                theme={theme}
-                title={theme}
-                icon={theme.includes('circle') ? 'microphone' : null}
-            >
-                {theme.includes('circle') ? null : theme}
+            <p>Icon after:</p>
+            <Button theme="secondary" iconAfter="off-site">
+                View calendar
             </Button>
-        ))}
-    </>
-));
+            <p>Icon before & after:</p>
+            <Button
+                theme="secondary"
+                iconBefore="off-site"
+                iconAfter="off-site"
+            >
+                View calendar
+            </Button>
+            <p>Icon only:</p>
+            <Button
+                theme="secondary"
+                aria-label="View calendar"
+                icon="calendar"
+            />
+        </>
+    ))
+    .add('Click', () => {
+        return (
+            <Button theme="secondary" onClick={action('clicked')}>
+                Click me
+            </Button>
+        );
+    });
